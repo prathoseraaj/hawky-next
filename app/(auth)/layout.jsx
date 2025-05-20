5dcaf4697e13cf8a855becf34387187e14f3575a
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-20 16:17:22 +0530

    feat: layout for auth

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,11 @@
+
+
+const AuthLayout = async ({ children }) => {
+  return (
+    <div className="flex items-center justify-center mx-auto max-w-7xl min-h-screen max-sm:px-4 max-sm:py-8">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
