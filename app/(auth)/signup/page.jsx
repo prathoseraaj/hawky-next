5c72208cca274ac2ac2a6694d93a3a0f063d782c
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-20 16:53:38 +0530

    feat: email and password

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>signup</div>
-  )
-}
+    <div className="h-[70vh] w-[500px] bg-white-100 flex flex-col  bg-white shadow-sm rounded-[20px]">
+      <div className="items-center">
+        <img
+          src="/logo.jpeg"
+          alt="logo"
+          height={500}
+          width={500}
+          className="mt-5"
+        />{" "}
+      </div>
+      <div className="flex flex-col ml-15 mt-10">
+        <label htmlFor="" className="font-bold text-[20px] ">
+          Email
+        </label>
+        <input type="email" className="w-[380px] h-[30px] border rounded  " />
 
-export default page
+        <label htmlFor="" className="font-bold text-[20px] ">
+          Password
+        </label>
+        <input type="text" className="w-[380px] h-[30px] border rounded  " />
+      </div>{" "}
+    </div>
+  );
+};
+
+export default page;
