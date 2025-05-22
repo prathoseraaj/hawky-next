ca2a46dabda25ecc3428d1f3a86f0e520ee3c851
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-22 13:15:57 +0530

    fix: signin and signup

--- app/(auth)/signin/page.jsx
+++ app/(auth)/signin/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>signin</div>
-  )
-}
+    <div className="h-[70vh] w-[500px] bg-white-100 flex flex-col  bg-white shadow-sm rounded-[20px]">
+      <div className="items-center">
+        <img
+          src="/logo.jpeg"
+          alt="logo"
+          height={500}
+          width={500}
+          className="mt-10 -mb-5"
+        />
+      </div>
+      <h1 className="text-[20px] font-bold ml-15 mt-10 -mb-5">Sign-In</h1>
+      <div className="flex flex-col gap-7 ml-15 mt-10">
+        <input type="email" className="w-[380px] h-[40px] border rounded  pl-5" placeholder="Email" />
+        <input type="text" className="w-[380px] h-[40px] border rounded pl-5  " placeholder="Password" />
+      </div>
+      <p className="ml-15 mt-5 text-[13px] font-bold text-green-600">Forgot Password?</p>
+      <button type="button" className="w-[380px] h-[40px] mt-5 text-white font-bold text-[17px] rounded ml-15 bg-green-500 cursor-pointer" >Login</button>
 
-export default page
+      <div className="flex flex-row w-[400px] ">
+        <input type="checkbox" name="terms" id="terms" className="ml-15 mt-5  " />
+        <p className="mt-5 text-[12px] ml-2 mt-5 text-gray-500">By countinuing, I agree to the terms of Use & Privacy policy   </p>
+      </div>
+      <div className="ml-15 mt-5 text-[13px]">
+        <p>Create an account?<span className="text-green-500 ml-1 font-bold">click here</span></p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
